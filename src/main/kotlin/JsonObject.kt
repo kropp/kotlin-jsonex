dc60ff9c6692a4e921e8ffe5363499c8e95d7d4e
Victor Kropp <victor.kropp@gmail.com>
2017-01-30 16:33:45 +0100

    simplify

--- src/main/kotlin/JsonObject.kt
+++ src/main/kotlin/JsonObject.kt
@@ -5,45 +5,29 @@ import java.util.*
 import kotlin.reflect.KProperty
 
 @JsonDslMarker
-abstract class MutableJsonObject<T : MutableJsonObject<T>>(val _map: MutableMap<String,Any> = mutableMapOf()) {
-  private companion object {
-    private val dateFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
-  }
+abstract class MutableJsonObject<T : MutableJsonObject<T>>(map: MutableMap<String,Any> = mutableMapOf()) : JsonObject<T>(map) {
+  public override val _map: MutableMap<String, Any> = map
 
-  override fun toString() = "{${_map.map { (k,v) -> "\"$k\":${toLiteral(v)}" }.joinToString(",")}}"
-  private fun toLiteral(v: Any?): String = when(v) {
-    null -> "null"
-    is String -> "\"$v\""
-    is Date -> "\"${dateFormat.format(v)}\""
-    is Array<*> -> "[${v.map { toLiteral(it) }.joinToString(",")}]"
-    is Map<*,*> -> "{${v.map { (k,v) -> "\"$k\":${toLiteral(v)}" }.joinToString(",")}}"
-    else -> v.toString()
-  }
-
-  operator fun get(key: String) = _map[key]
   operator fun set(key: String, value: Any) { _map[key] = value }
 
-  private lateinit var _parent: MutableJsonObject<*>
-  private lateinit var _name: String
-
   operator fun provideDelegate(o: MutableJsonObject<*>, property: KProperty<*>): MutableJsonObject<T> {
-    _parent = o
-    _name = property.name
-    if (_name in _parent) {
-      (_parent[_name] as? Map<*,*>)?.forEach { k, v -> if (k is String && v != null) this[k] = v }
+    if (property.name in o) {
+      val v = o[property.name]
+      when(v) {
+        is MutableJsonObject<*> -> _map.putAll(v._map)
+        is Map<*, *> -> _map.putAll(v as Map<out String, Any>)
+      }
     }
-    _parent[_name] = this
+    o[property.name] = this
     return this
   }
 
-  operator fun invoke(body: T.() -> Unit) { (_parent[_name] as T).apply(body) }
-  operator fun contains(key: String) = key in _map
-  operator fun getValue(o: MutableJsonObject<*>, property: KProperty<*>): T = o[property.name] as T
+  operator fun invoke(body: T.() -> Unit) { (this as T).apply(body) }
   operator fun setValue(o: MutableJsonObject<*>, property: KProperty<*>, value: T) { o[property.name] = value }
 }
 
 abstract class JsonObject<T : JsonObject<T>>(map: Map<String,Any> = mapOf()) {
-  private val _map: MutableMap<String,Any> = LinkedHashMap(map)
+  protected open val _map: MutableMap<String,Any> = LinkedHashMap(map)
 
   private companion object {
     private val dateFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
@@ -61,16 +45,15 @@ abstract class JsonObject<T : JsonObject<T>>(map: Map<String,Any> = mapOf()) {
 
   operator fun get(key: String) = _map[key]
 
-  private lateinit var _parent: JsonObject<*>
-  private lateinit var _name: String
-
   operator fun provideDelegate(o: JsonObject<*>, property: KProperty<*>): JsonObject<T> {
-    _parent = o
-    _name = property.name
-    if (_name in _parent) {
-      (_parent[_name] as? Map<*,*>)?.forEach { k, v -> if (k is String && v != null) _map[k] = v }
+    if (property.name in o) {
+      val v = o[property.name]
+      when(v) {
+        is JsonObject<*> -> _map.putAll(v._map)
+        is Map<*, *> -> _map.putAll(v as Map<out String, Any>)
+      }
     }
-    _parent._map[_name] = this
+    o._map[property.name] = this
     return this
   }
 
